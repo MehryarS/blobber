8921eaf032f6fc0ae8c0c82aad414d63460e6b78
MEHRYAR.S <me@mehryars.com>
2018-02-24 21:23:43 +0330

    version 2.0 - progress bar added to plugin

--- src/js/blobber.js
+++ src/js/blobber.js
@@ -4,54 +4,86 @@
 
         var i = 1;
 
+        //Default settings
         var settings = $.extend({
-            complete     : null
+
+            complete     : null,
+            progressbar  :  true
+
         }, options);
 
         $(this).find("video").each(function() {
             var video_url = $(this).attr("src");
             var video_id = "#" + $(this).attr("id");
+            var video_total_count = $("video").length;
+            var percnet = 0;
 
-            var req = new XMLHttpRequest();
-            req.open('GET', video_url , true);
-            req.responseType = 'blob';
+            function loadFile(url, callback) {
+                var req = new XMLHttpRequest();
+                req.callback = callback;
+                req.arguments = Array.prototype.slice.call(arguments, 2);
+                req.onload = reqSuccess;
+                req.onerror = reqError;
+                req.open("GET", url, true);
+                req.responseType = 'blob';
 
-            req.onload = function() {
-
-                if (this.status === 200) {
-                    var videoBlob = this.response;
-                    var vid = URL.createObjectURL(videoBlob);
-
-                    //New URI loaded in DOM
-                    $(video_id).attr("src", vid);
-
-                    //Console progress bar
-                    var video_count = $("video").length;
-
-                    if(video_count > 1){
-                        console.log("Downloading Videos: " + i + " of " + video_count + " Videos Downloaded. (Ready at " + video_count + ")");
+                req.addEventListener("progress", updateProgress, false);
+                function updateProgress (event) {
+                    if (event.lengthComputable) {
+                        percnet = ((event.loaded / event.total) / video_total_count) * 100;
                     } else {
-                        console.log("Downloading Videos: 1 Video is Downloaded.");
-                    }
-
-                    i = i + 1;
-
-                    if(i == (video_count + 1)){
-
-                        //if Callback function is defined
-                        if ( $.isFunction( settings.complete ) ) {
-                            settings.complete.call( this );
-                        } else {
-                            console.log("Hooray! We're Ready to Fire.");
-                        }                        
+                        console.log("We have an error");
                     }
                 }
-            };
-            req.onerror = function() {
-                console.log(":-( Sorry! We have an error.");
-            };
 
-            req.send();
+                function reqSuccess() { 
+                    this.callback.apply(this, this.arguments); 
+                    if (req.readyState == 4 && req.status == 200) {
+                        var videoBlob = this.response;
+                        var vid = URL.createObjectURL(videoBlob);
+    
+                        //New URI loaded in DOM
+                        $(video_id).attr("src", vid);
+                    } else {
+                        console.error(req.statusText);
+                    }
+                }
+                
+                function reqError() { 
+                    console.error(this.statusText); 
+                    console.log(":-( Sorry! We have an error.");
+                }
+                req.send(null);
+            }
+
+            function showMessage(message) {
+
+                //Progress bar
+                var progressbar_width = (($(".progress").width() / $(document).width()) * 100) + percnet;
+                $(".progress").animate({width: progressbar_width + "%"});
+
+                //Console progress notification
+                if(video_total_count > 1){
+                    console.log("Downloading Videos: " + i + " of " + video_total_count + " Videos Downloaded. (Ready at " + video_total_count + ")");
+                } else {
+                    console.log("Downloading Videos: 1 Video is Downloaded.");
+                }
+
+                i = i + 1;
+
+                if(i == (video_total_count + 1)){
+
+                    //Check callback function is defined
+                    if ( $.isFunction( settings.complete ) ) {
+                        settings.complete.call( this );
+                    } else {
+                        console.log("Hooray! We're Ready to Fire.");
+                        $(".progress").fadeOut();
+                    }                        
+                }
+            }
+
+            loadFile(video_url, showMessage);
         });
     };
 
