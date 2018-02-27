1a696ec832889f9f07539e44598f6b8517ba7c20
MEHRYAR.S <me@mehryars.com>
2018-02-28 01:43:55 +0330

    version 2.5 - now preloader is customizable by user

--- src/js/blobber.js
+++ src/js/blobber.js
@@ -1,7 +1,5 @@
 (function($) {
 
-    // $("body").append("<div class='progress'></div>");
-
     $.fn.blobber = function(options) {
 
         var i = 1;
@@ -9,15 +7,39 @@
         //Default settings
         var settings = $.extend({
 
-            complete     : null,
-            progressbar  :  true
+            complete                :   null,
+            preloader               :   true,
+            preloaderBackground     :   "#FFFFFF",
+            progressBar             :   true,
+            progressBarColor        :   "#000000",
+            progressSpinner         :   true,
+            progressSpinnerColor    :   "#000000",
+            progressStatus          :   true,
+            progressStatusColor     :   "#999999",
+            preloaderAnimation      :   null
 
         }, options);
 
+        if ( settings.preloader == true ) {
+            $("body").append('<div class="progress" style="background: ' + settings.preloaderBackground  + '"></div>');
+        }
+
+        if ( settings.progressBar == true ) {
+            $(".progress").append('<div class="progress-bar" style="background: ' + settings.progressBarColor  + '"></div>');
+        }
+
+        if ( settings.progressSpinner == true ) {
+            $(".progress").append('<div class="progress-animation" style="border-color: ' + settings.preloaderBackground + '; border-top-color: ' + settings.progressSpinnerColor  + '"></div>');
+        }
+
+        if ( settings.progressStatus == true ) {
+            $(".progress").append('<div class="progress-status" style="color: ' + settings.progressStatusColor  + '"><span></span></div>');
+        }
+
         $(this).find("video").each(function() {
-            var video_url = $(this).attr("src");
-            var video_id = "#" + $(this).attr("id");
-            var video_total_count = $("video").length;
+            var videoURL = $(this).attr("src");
+            var videoID = "#" + $(this).attr("id");
+            var videoTotalCount = $("video").length;
             var percnet = 0;
 
             function loadFile(url, callback) {
@@ -32,7 +54,7 @@
                 req.addEventListener("progress", updateProgress, false);
                 function updateProgress (event) {
                     if (event.lengthComputable) {
-                        percnet = ((event.loaded / event.total) / video_total_count) * 100;
+                        percnet = ((event.loaded / event.total) / videoTotalCount) * 100;
                     } else {
                         console.log("We have an error");
                     }
@@ -45,7 +67,7 @@
                         var vid = URL.createObjectURL(videoBlob);
     
                         //New URI loaded in DOM
-                        $(video_id).attr("src", vid);
+                        $(videoID).attr("src", vid);
                     } else {
                         console.error(req.statusText);
                     }
@@ -58,26 +80,38 @@
                 req.send(null);
             }
 
+            function changeStatus(status){
+
+                $(".progress-status span").fadeOut(function(){
+                    $(this).text("");
+                    $(this).text(status);
+                    $(this).fadeIn();
+                });
+            }
+
             function showMessage(message) {
 
                 //Progress bar
-                var progressbar_width = (($(".progress").width() / $(document).width()) * 100) + percnet;
-                $(".progress").animate({width: progressbar_width + "%"});
+                var progressbarWidth = (($(".progress-bar").width() / $(document).width()) * 100) + percnet;
+                $(".progress-bar").animate({width: progressbarWidth + "%"});
 
                 //Console progress notification
-                if(video_total_count > 1){
-                    console.log("Downloading Videos: " + i + " of " + video_total_count + " Videos Downloaded. (Ready at " + video_total_count + ")");
+                if(videoTotalCount > 1){
+                    console.log("Downloading Videos: " + i + " of " + videoTotalCount + " Videos Downloaded. (Ready at " + videoTotalCount + ")");
+                    changeStatus("Downloading Videos: " + i + " of " + videoTotalCount + " Videos Downloaded. (Ready at " + videoTotalCount + ")");
                 } else {
-                    console.log("Downloading Videos: 1 Video is Downloaded.");
+                    changeStatus("Downloading Videos: Video is Downloaded.");
+                    console.log("Downloading Videos: Video is Downloaded.");
                 }
 
                 i = i + 1;
 
-                if(i == (video_total_count + 1)){
+                if(i == (videoTotalCount + 1)){
 
                     //Check callback function is defined
                     if ( $.isFunction( settings.complete ) ) {
                         settings.complete.call( this );
+                        $(".progress").fadeOut();
                     } else {
                         console.log("Hooray! We're Ready to Fire.");
                         $(".progress").fadeOut();
@@ -85,7 +119,7 @@
                 }
             }
 
-            loadFile(video_url, showMessage);
+            loadFile(videoURL, showMessage);
         });
     };
 
